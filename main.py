5d9f3885484444ce4311f1cc6c312659793a4e54
ZCJ <1689324109@qq.com>
2024-08-29 17:08:44 +0800

    完成签到脚本

--- main.py
+++ main.py
@@ -1,16 +1,86 @@
-# 这是一个示例 Python 脚本。
+# coding=utf-8
+#! /usr/bin/env python# -*- coding: utf-8 -*-
+import requests, json, re, os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+session = requests.session()
+# 配置用户名（一般是邮箱）
+username = os.environ.get('USERNAME')
+# username = "1689324109"
+# 配置用户名对应的密码 和上面的email对应上
+password = os.environ.get('PASSWORD')
+# password = "zcj.080818"
+# AnPlus
+# AnPlus = os.environ.get('AnPlus')
+AnPlus = 'J9C9I7V7RI4YEPHHRDHB2WL137ZDAY'
+def push(content):
+    if AnPlus != '1' :
+        payload = {
+            "title": u"ikuuu签到:"+content,
+            "content": content,
+            "channel": "97024"
+        }
 
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+        response = requests.post("https://api.anpush.com/push/"+AnPlus, headers=headers, data=payload)
+        print('AnPlus消息推送推送成功' if response.status_code == 200 else 'AnPlus消息推送推送失败')
+    else:
+        print('未使用消息推送推送！')
+def getCsrf(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
 
+    pattern = re.compile(r'\?page=logout&csrf=([a-zA-Z0-9]+)')
+    match = pattern.search(html_content)
+    csrf_token = match.group(1)
+    return csrf_token
+# 会不定时更新域名，记得Sync fork
+# 定义请求的 URL
+loginUrl = "https://xhfrp.xhuzim.top/?action=login&page=login"
+userInfoUrl = "https://xhfrp.xhuzim.top/?page=panel"
+checkPageUrl = "https://xhfrp.xhuzim.top/?page=panel&module=sign"
+checkUrl = 'https://xhfrp.xhuzim.top/?page=panel&module=sign&sign&csrf='
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 定义请求的头部
+headers = {
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "upgrade-insecure-requests": "1",
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'origin': 'https://xhfrp.xhuzim.top'
+}
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+# 定义请求的 body
+data = {
+    "g-recaptcha-response": "",
+    "username": username,
+    "password": password
+}
+
+# 发送 POST 请求
+response = session.post(loginUrl, headers=headers, data=data)
+print(response)
+info = session.get(userInfoUrl, headers=headers)
+token = getCsrf(info.text)
+checkPag = session.get(checkPageUrl, headers=headers)
+print(checkPag)
+check = session.get(checkUrl + token, headers=headers)
+print(check.text)
+push(check.text)
+#     print(response['msg'])
+#     # 获取账号名称
+#     info_html = session.get(url=info_url,headers=header).text
+# #     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
+# #     print(info)
+#     # 进行签到
+#     result = json.loads(session.post(url=check_url,headers=header).text)
+#     print(result['msg'])
+#     content = result['msg']
+#     # 进行推送
+#     push(content)
+
